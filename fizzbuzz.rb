06b5b81d49c26efe7de58efbbb302ea4c3e5bf07
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 12:23:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(integer)
-  if (integer % 3 == 0 && integer % 5 == 0)
+def fizzbuzz(int)
+  if (int % 3 == 0 && int % 5 == 0)
     return "FizzBuzz"
-  elsif (integer % 3 == 0)
+  elsif (int % 3 == 0)
     return "Fizz"
-  elsif (integer % 5 == 0)
+  elsif (int % 5 == 0)
     return "Buzz"
   else
     return nil
