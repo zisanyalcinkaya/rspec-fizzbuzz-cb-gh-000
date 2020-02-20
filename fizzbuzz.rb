3275d50ce3138a4c5fe2215c65f2e7b226850e39
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 11:09:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" 
+    "Fizz"
    end
     if int % 5 == 0 # if the number int is divisible by 3
-        "Buzz"    
+        "Buzz"
     end
         if int % 15 == 0
           "FizzBuzz"
