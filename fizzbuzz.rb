765bebd2e4c0d94b52b8eb0b36e47dbf44cf0fb8
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 11:22:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'pry'
-
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz"
    end
+    if int % 5 == 0 # if the number int is divisible by 3
+        "Buzz"
+    end
+        if int % 15 == 0
+          "FizzBuzz"
+        end
+
 end
