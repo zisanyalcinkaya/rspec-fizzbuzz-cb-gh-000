006f31fd254fe5331e986a3adf3521d61f8ab4e0
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-20 12:23:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-  return
-
-    "Fizz"
-
- elseif int % 5 == 0 # if the number int is divisible by 3
-    return    "Buzz"
-elseif (int % 5 == && int %3==0)
-  return  "FizzBuzz"
-else
-  return nil
+def fizzbuzz(integer)
+  if (integer % 3 == 0 && integer % 5 == 0)
+    return "FizzBuzz"
+  elsif (integer % 3 == 0)
+    return "Fizz"
+  elsif (integer % 5 == 0)
+    return "Buzz"
+  else
+    return nil
+  end
 end
-   end
